f09412d1c3498445ac6718949967899aa710ae27
Hugo Laloge <hugo.laloge@gmail.com>
2018-03-09 12:44:43 +0000

    feat: Add all-steps compilation

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,8 @@ extern crate memchr;
 #[macro_use] extern crate nom;
 
 use std::fs::File;
-use std::io::{Read, Write};
+use std::io::Read;
+use std::process::{Child, Command, Stdio};
 
 pub mod c;
 pub mod parser;
@@ -14,26 +15,30 @@ use c::Compile;
 pub fn compile_file(input_file: &str, output_file: &str) -> Result<(), &'static str> {
     let mut input = File::open(input_file).map_err(|_| "Invalid file")?;
 
-    if output_file == "-" {
-        compile(&mut input, &mut std::io::stdout())
-    } else {
-        let mut output = File::create(output_file).map_err(|_| "Invalid file")?;
-        compile(&mut input, &mut output)
-    }.map_err(|_| "Compilation_error")
+    let ast = get_ast(&mut input)
+        .map_err(|_| "Compilation error")?;
+    let mut child = get_cc_command(output_file);
+    ast.compile(child.stdin.as_mut().expect("Failed to open stdin"))
+        .map_err(|_| "Write error")
 }
 
-fn compile<I, O>(input: &mut I, output: &mut O) -> Result<(), ()> where I:Read, O: Write {
+fn get_cc_command(output_file: &str) -> Child {
+    Command::new("cc")
+        .stdin(Stdio::piped())
+        .args(&["-x", "assembler"])
+        .arg("-m32")
+        .args(&["-o", output_file])
+        .arg("-")
+        .spawn()
+        .expect("Failed to execute command")
+}
+
+fn get_ast<I>(input: &mut I) -> Result<c::Function, ()> where I: Read {
     let mut data = String::new();
     input.read_to_string(&mut data).unwrap();
 
-    let function = match parser::parse(&data) {
-        Ok(func) => func,
-        Err(e)   => {
-            eprintln!("{}", e);
-            return Err(());
-        },
-    };
-    function.compile(output).unwrap();
-
-    Ok(())
+    parser::parse(&data).map_err(|e| {
+        eprintln!("{}", e);
+        ()
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #[macro_use] extern crate clap;
+extern crate memchr;
 extern crate rcc;
 
 use clap::{Arg, App};
@@ -13,14 +14,36 @@ fn main() {
              .required(true)
              .index(1))
         .arg(Arg::with_name("OUTPUT")
+             .long("output")
+             .short("o")
              .help("Output file")
-             .default_value("-")
-             .index(2))
+             .takes_value(true))
         .get_matches();
 
     let input_file = matches.value_of("INPUT").unwrap();
-    let output_file = matches.value_of("OUTPUT").unwrap();
+    let default_output = get_output_file_name(input_file);
+    let output_file = matches.value_of("OUTPUT").unwrap_or(&default_output);
     if rcc::compile_file(input_file, output_file).is_err() {
         ::std::process::exit(1);
+    }
+}
+
+fn get_output_file_name(input_file_name: &str) -> String {
+    if let Some(idx) = input_file_name.rfind('.') {
+        &input_file_name[..idx]
+    } else {
+        "a.out"
+    }.to_owned()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_output_file_name() {
+        assert_eq!(get_output_file_name("test.c"), "test");
+        assert_eq!(get_output_file_name("-"), "a.out");
+        assert_eq!(get_output_file_name(".src/test.c"), ".src/test");
     }
 }
